fbe42b31ab7b444a9bb2b90ca2ab59ed4dc2433c
TDM <dtazetdinov1998@gmail.com>
2026-02-19 10:21:54 +0700

    fix

--- back/app/main.py
+++ back/app/main.py
@@ -2,11 +2,37 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from back.app.api import items
 
-app = FastAPI()
+app = FastAPI(
+    title="EZ Family Task",
+    description="""
+    Тестовое задание.
+
+    ## Возможности
+    * **Создание** новых элементов
+    * **Просмотр** списка элементов
+    * **Обновление** существующих элементов
+    * **Удаление** элементов
+
+    Все текстовые поля проходят валидацию:
+    * Не могут быть пустыми
+    * Не длиннее 50 символов
+    * Должны быть уникальными
+    """,
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    contact={
+        "name": "tazetdinov",
+        "email": "dtazetdinov1998@gmail.com",
+    },
+    license_info={
+        "name": "MIT",
+    },
+)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://192.168.100.14:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
